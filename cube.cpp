ca4806a17877d38ad174d83ac334dece566fec63
AdamPCST2550 <githubcst2550@gmail.com>
2023-11-01 21:39:34 +0000

    First commit

---
+++ cube.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int cubeNumber(int number); 
+
+int main(){
+
+  int x;
+  
+  cout<<"Enter an integer\n>>";
+  cin>> x;
+
+  
+
+  cout<<x << " cubed is " << cubeNumber(x);
+  
+  return 0;
+}
+
+int cubeNumber(int number){
+
+  return (number * number) * number;
+  
+}
